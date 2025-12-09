c7ee15896ef9d4ae2bd6c33d185c59e8cea7c8c4
Pablo1337PL <janp.taran@gmail.com>
2025-12-09 18:46:40 +0100

    deleting

--- testing.py
+++
@@ -1,9 +0,0 @@
-from sklearn.base import ClassifierMixin
-from sklearn.utils import all_estimators
-classifiers=[est for est in all_estimators() if issubclass(est[1], ClassifierMixin)]
-
-# Print all classifier algorithms to file testing.txt
-with open("testing.txt", "w") as f:
-    for clas in classifiers:
-        print(clas[0])
-        f.write(f"{clas[0]}\n")
