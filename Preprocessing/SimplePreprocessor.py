b1ff40b90c416b10a5ead54b73fe5e05421a7176
Pablo1337PL <janp.taran@gmail.com>
2025-12-11 21:33:25 +0100

    changed simple preproc

--- Preprocessing/SimplePreprocessor.py
+++ Preprocessing/SimplePreprocessor.py
@@ -3,51 +3,53 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 class SimplePreprocessor:
-    def __init__(self, target_col='target'):
-        self.target_col = target_col
-        
-        # Słowniki do przechowywania "wiedzy" z treningu
-        self.means = {}           # Średnie dla zmiennych ciągłych
-        self.modes = {}           # Mody dla zmiennych kategorycznych
-        self.cat_encoders = {}    # Mapowania kategoryczne (LabelEncoders)
+    """
+    Najprostszy processing: uzupełnianie braków, skalowanie numerycznych, kodowanie kategorycznych.
+    """
+    def __init__(self):
+        # Magazyn wiedzy (statystyki z treningu)
+        self.means = {}
+        self.modes = {}
+        self.cat_encoders = {} # Słownik: {kolumna: {'encoder': le, 'classes': set(...)}}
+        self.y_encoder = None
         self.scaler = StandardScaler()
         
         self.cat_cols = []
         self.num_cols = []
         self.is_fitted = False
 
-    def fit(self, df):
+    def fit(self, X, y):
         """
-        Uczy się statystyk (średnie, kategorie) TYLKO na zbiorze treningowym.
+        Tylko 'patrzy' na dane i zapamiętuje statystyki. Nie zwraca zmienionych danych.
         """
-        X = df.drop(columns=[self.target_col])
-        
         # 1. Automatyczna detekcja kolumn
         self.cat_cols = X.select_dtypes(include=['object', 'category']).columns.tolist()
         self.num_cols = X.select_dtypes(include=['number']).columns.tolist()
 
-        # 2. Nauka na zmiennych numerycznych
+        # 2. Target Encoder
+        self.y_encoder = LabelEncoder()
+        self.y_encoder.fit(y)
+
+        # 3. Zmienne Numeryczne: Zapamiętaj średnie i naucz skaler
         for col in self.num_cols:
             self.means[col] = X[col].mean()
         
-        # Wstępne uzupełnienie do fitowania skalera
-        X_num = X[self.num_cols].fillna(X[self.num_cols].mean())
-        self.scaler.fit(X_num)
+        # Uczymy skaler na tymczasowej kopii (żeby nie psuć X w miejscu)
+        if self.num_cols:
+            X_num_temp = X[self.num_cols].fillna(X[self.num_cols].mean())
+            self.scaler.fit(X_num_temp)
+            del X_num_temp
 
-        # 3. Nauka na zmiennych kategorycznych
+        # 4. Zmienne Kategoryczne: Zapamiętaj mody i naucz encodery
         for col in self.cat_cols:
-            # Zapamiętujemy modę
             mode_val = X[col].mode()[0] if not X[col].mode().empty else 'missing'
             self.modes[col] = mode_val
             
-            # Wypełniamy braki modą tymczasowo do nauki enkodera
-            X[col] = X[col].fillna(mode_val).astype(str)
-            
-            # Uczymy LabelEncodera
             le = LabelEncoder()
-            le.fit(X[col])
+            # Uczymy na uzupełnionych danych rzutowanych na string
+            col_data = X[col].fillna(mode_val).astype(str)
+            le.fit(col_data)
             
-            # Zapisujemy klasy, żeby obsłużyć nieznane wartości w transform
             self.cat_encoders[col] = {
                 'encoder': le,
                 'classes': set(le.classes_)
@@ -56,41 +58,51 @@ class SimplePreprocessor:
         self.is_fitted = True
         return self
 
-    def transform(self, df):
+    def transform(self, X, y):
         """
-        Aplikuje zapamiętane statystyki na dane (treningowe lub testowe).
+        Aplikuje wiedzę z fit() na dowolny zbiór (treningowy lub testowy).
+        Zwraca NOWE, zmodyfikowane ramki danych.
+        Zrobione do użytku na zbiorze testowym.
         """
         if not self.is_fitted:
-            raise Exception("Najpierw uruchom fit() na zbiorze treningowym!")
-            
-        data = df.copy()
+            raise Exception("Najpierw użyj fit() na zbiorze treningowym!")
         
-        # Obsługa targetu (jeśli istnieje, np. w treningu/teście, ale nie na produkcji)
-        y = None
-        if self.target_col in data.columns:
-            y = data[self.target_col].values # Zwracamy jako numpy array
-            data = data.drop(columns=[self.target_col])
+        X_trans = X.copy()
+        y_trans = y.copy()
         
-        # 1. Numeryczne: Uzupełnianie średnią z treningu + Skalowanie
+        # 1. Numeryczne
         for col in self.num_cols:
-            if col in data.columns:
-                data[col] = data[col].fillna(self.means[col])
+            if col in X_trans.columns:
+                X_trans[col] = X_trans[col].fillna(self.means[col])
         
         if self.num_cols:
-            data[self.num_cols] = self.scaler.transform(data[self.num_cols])
+            X_trans[self.num_cols] = self.scaler.transform(X_trans[self.num_cols])
 
-        # 2. Kategoryczne: Uzupełnianie modą z treningu + Kodowanie
+        # 2. Kategoryczne
         for col in self.cat_cols:
-            if col in data.columns:
+            if col in X_trans.columns:
                 mode_val = self.modes[col]
-                data[col] = data[col].fillna(mode_val).astype(str)
+                # Uzupełnianie i rzutowanie
+                X_trans[col] = X_trans[col].fillna(mode_val).astype(str)
                 
-                # Bezpieczne kodowanie (handle unknown)
+                # Bezpieczne kodowanie (nieznane wartości -> moda)
                 le = self.cat_encoders[col]['encoder']
                 known_classes = self.cat_encoders[col]['classes']
                 
-                # Jeśli wartość nie była w treningu, zamień na modę (lub 'other')
-                data[col] = data[col].apply(lambda x: x if x in known_classes else mode_val)
-                data[col] = le.transform(data[col])
+                # Szybka funkcja mapująca (lambda)
+                X_trans[col] = X_trans[col].apply(lambda x: x if x in known_classes else mode_val)
+                X_trans[col] = le.transform(X_trans[col])
 
-        return data, y
+        # 3. Target
+        if self.y_encoder is None:
+            raise Exception("Nie nauczono encodera dla y (fit wywołano bez y).")
+        y_trans = self.y_encoder.transform(y_trans)
+        
+        return X_trans, y_trans
+
+    def fit_transform(self, X, y):
+        """
+        Uczy się I od razu zwraca zmieniony zbiór.
+        Zrobione do użycia na zbiorze treningowym.
+        """
+        return self.fit(X, y).transform(X, y)
