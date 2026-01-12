658e4ebf7962533a8faddd4faaf34a6ec0d0644c
Pablo1337PL <janp.taran@gmail.com>
2026-01-12 20:16:45 +0100

    One Preprocessor to rule them all

--- Preprocessing/SimplePreprocessor.py
+++ Preprocessing/SimplePreprocessor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
 
 class SimplePreprocessor:
     """
@@ -17,6 +18,26 @@ class SimplePreprocessor:
         self.cat_cols = []
         self.num_cols = []
         self.is_fitted = False
+
+    def process(self, X, y, test_size=0.2, random_state=42):
+        """
+        Pełny proces: fit + transform.
+        Zwraca NOWE, zmienione ramki danych.
+        """
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+        X_train, y_train = self.fit_transform(X_train, y_train)
+        X_test, y_test = self.transform(X_test, y_test)
+        
+
+
+
+
+
+
+
+
+        return X_train, X_test, y_train, y_test
+
 
     def fit(self, X, y):
         """
